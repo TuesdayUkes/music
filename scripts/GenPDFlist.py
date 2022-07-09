ad0350b337fdb80777b9c07b58964c3d818d42f6
Paul Butler <c.paul.butler@gmail.com>
2022-07-08 21:52:54 -0500

    Add formatting to links HTML

--- scripts/GenPDFlist.py
+++ scripts/GenPDFlist.py
@@ -5,18 +5,24 @@ import os
 # lambda l accepts a path and returns just the filename without an extension
 l = lambda p: str(os.path.splitext(os.path.basename(p))[0])
 
-allFiles = []
+
 # for p in Path("./").rglob('*.chopro'):
 #   allFiles.append(p)
 # 
 # for p in Path("./").rglob('*.cho'):
 #   allFiles.append(p)
 
+with open("scripts/HTMLheader.txt", "r") as headerText:
+  header = headerText.readlines()
+
+allPdfFiles = []
 for p in Path("./").rglob('*.pdf'):
-  allFiles.append(p)
+  allPdfFiles.append(p)
 
-sortedFiles = sorted(allFiles, key=(lambda p: l(p).casefold()))
+sortedFiles = sorted(allPdfFiles, key=(lambda p: l(p).casefold()))
 
-htmlOutput = open("PDFLinks.html", "w")
-for f in sortedFiles:
-  htmlOutput.write(f"<a href=\"{str(f.resolve())}\" download>{l(f)}</a><br>")
+with open("PDFLinks.html", "w") as htmlOutput:
+  htmlOutput.writelines(header)
+  for f in sortedFiles:
+    htmlOutput.write(f"<a href=\"{str(f.resolve())}\" download>{l(f)}</a><br>\n")
+  htmlOutput.write("</body>\n")

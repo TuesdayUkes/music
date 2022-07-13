7d3911883b80059ffacfbb0cfc5a1b3bee84a0bb
Paul Butler <c.paul.butler@gmail.com>
2022-07-13 16:45:22 -0500

    Moving history of video index to website

--- scripts/formatIndex.py
+++ scripts/formatIndex.py
@@ -12,21 +12,32 @@ args = parser.parse_args()
 
 inputFilename = args.inputFilename
 youtubeLink = args.youtubeLink
-outputFilename = "VideoIndex.js"
+JsFilename = "VideoIndex.js"
+HtmlFilename = "VideoIndex.html"
 
 with open(inputFilename, "r") as sourceText:
   sourceLines = sourceText.readlines()
 
-with open(outputFilename, "w") as outFile:
-  outFile.write("document.write('\\")
-  outFile.write("\n")
-  for l in sourceLines:
-    newL = re.sub("^(\d+):(\d+):(\d+)", f"<a href=\"{youtubeLink}?t=\\1h\\2m\\3s\">\\1:\\2:\\3</a>",l)
-    newL = re.sub("^(\d+):(\d+)", f"<a href=\"{youtubeLink}?t=\\1m\\2s\">\\1:\\2</a>",newL)
-    title = re.search(r'\(.*\)',l).group(0)
-    title = re.sub(r"'",r"\\'",title)
-    # newL = re.sub(r"\(.*\)'", r"",newL) # remove title
-    newL = re.sub(r"'", r"\\'",newL)
-    newL = re.sub("\((.*)\) (https?:.*)", r"<a href=\2>\1</a>",newL)
-    outFile.write(newL.rstrip()+"<br>\\\n")
-  outFile.write(r"')")
+with open(HtmlFilename, "w") as HtmlFile:
+  with open(JsFilename, "w") as JsFile:
+    JsFile.write("document.write('\\")
+    JsFile.write("\n")
+    for l in sourceLines:
+      # For timestamps greater than an hour, look for three numbers separated by 2 colons
+      newL = re.sub("^(\d+):(\d+):(\d+)", f"<a href=\"{youtubeLink}?t=\\1h\\2m\\3s\">\\1:\\2:\\3</a>",l)
+
+      # For timestamps less than an hour, there are only two time fields (minutes : seconds)
+      newL = re.sub("^(\d+):(\d+)", f"<a href=\"{youtubeLink}?t=\\1m\\2s\">\\1:\\2</a>",newL)
+
+      # reshape URL for sheet music into an HTML tag using the song title as the
+      # visible text
+      newL = re.sub("\((.*)\) (https?:.*)", r"<a href=\2>\1</a>",newL)
+
+      HtmlFile.write(newL.rstrip()+"<br>\n")
+
+      # Add escape backslashes in front of single quotes (also used as apostrophe)
+      # for the sake of the .js document.write string
+      newL = re.sub(r"'", r"\\'",newL)
+
+      JsFile.write(newL.rstrip()+"<br>\\\n")
+    JsFile.write(r"')")

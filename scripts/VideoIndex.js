9ced4a1c49aeaab9a84665c9e783e804fb07f3cb
Paul Butler <c.paul.butler@gmail.com>
2022-08-17 10:56:00 -0500

    new video index

--- scripts/VideoIndex.js
+++ scripts/VideoIndex.js
@@ -1,14 +1,12 @@
 document.write('\
 <table>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=16m30s">16:30</a><td>Mary Jane <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Someday%20Soon.pdf>Someday Soon</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=21m46s">21:46</a><td>Walter <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/KC%20Moan.pdf>KC Moan</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=30m28s">30:28</a><td>Brenda <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Spring%202022/House%20of%20the%20Rising%20Sun%20-%20Scroll.pdf>House of the Rising Sun</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=36m32s">36:32</a><td>Errol </td><td>Moon River</td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=42m14s">42:14</a><td>Christine </td><td>The River</td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=49m33s">49:33</a><td>Gary and Paulette <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Spring%202022/Deep%20in%20the%20West.pdf>Deep in the West</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=55m28s">55:28</a><td>Mary Jane <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Winter%202020-21/My%20Sweet%20Wyoming%20Home.pdf>My Sweet Wyoming Home</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=1h00m57s">1:00:57</a><td>Paul </td><td>Strawberry Fields Forever</td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=1h08m16s">1:08:16</a><td>group <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Proud%20Mary.pdf>Proud Mary</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=1h17m15s">1:17:15</a><td>group <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Stand%20by%20Me.pdf>Stand by Me</a></td></td></tr>\
-<tr><td><a href="https://youtu.be/3-mX8tpZNDA?t=1h28m13s">1:28:13</a><td>group <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=17m55s">17:55</a><td>Brenda <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Wayfaring%20Stranger.pdf>Wayfaring Stranger</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=24m05s">24:05</a><td>Mary Jane <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Bound%20for%20Freedom.pdf>Bound for Freedom</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=29m10s">29:10</a><td>Walter <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Bye%20Bye%20Blues.pdf>Bye Bye Blues</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=35m05s">35:05</a><td>Gary and Paulette <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202021/I\'m%20so%20Lonesome%20I%20Could%20Cry.pdf>I\'m So Lonesome I Could Cry</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=38m44s">38:44</a><td>Paul <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202021/Fire%20and%20Rain.pdf>Fire and Rain</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=44m55s">44:55</a><td>Vany <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Winter%202020-21/Blueberry%20Hill.pdf>Blueberry Hill</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=50m05s">50:05</a><td>Mary Jane <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Spring%202021/All%20God\'s%20Children%20Ain\'t%20Free.pdf>All God\'s Children Ain\'t Free</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=1h00m34s">1:00:34</a><td>group <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Proud%20Mary.pdf>Proud Mary</a></td></td></tr>\
+<tr><td><a href="https://youtu.be/8aj1-M7yYwc?t=1h18m20s">1:18:20</a><td>group <td><a href=http://69.5.217.253/TuesdayUkes/music/PDFs/Summer%202022/Happy%20Trails.pdf>Happy Trails</a></td></td></tr>\
 </table>')
